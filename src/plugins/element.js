c3fd3414c7f19cf3512590254fad2f406dde5300
taixian <yung2mao@126.com>
2020-06-16 09:33:23 +0800

    登录功能实现

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
+// eslint-disable-next-line import/no-duplicates
 import { Button } from 'element-ui'
+// eslint-disable-next-line no-unused-vars,import/no-duplicates
+import { Message } from 'element-ui'
+import { MessageBox } from 'element-ui';
 
 Vue.use(Button)
+Vue.prototype.$message = Message
+Vue.prototype.$messageBox = MessageBox
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import login from '../components/login.vue'
+import dog from '../components/dog'
+import MessageBox from 'element-ui/packages/message-box/src/main'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/login', component: login }
+  { path: '/login', component: login },
+  { path: '/dog', component: dog }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to,from,next) => {
+  if(to.path == '/login'){
+    return next()
+  }
+  const token = window.localStorage.getItem('token')
+  if(!token){
+    next('/login')
+  }else {
+    next()
+  }
 })
 
 export default router
